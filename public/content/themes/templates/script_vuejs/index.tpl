1da06c459c89f016480fa1d26e32662ad422f780
tsubit <nekofuranshisuko@gmail.com>
2021-03-07 20:24:18 -0300

    Revert "Consumo json por vuejs"
    This reverts commit 8185ba06401d8cebdc32e555df85fcf852b37322.

--- public/content/themes/templates/script_vuejs/index.tpl
+++ public/content/themes/templates/script_vuejs/index.tpl
@@ -1,30 +1,7 @@
 <script>
 
-var fecha = "{$fecha}";
-var archivo = "{$archivo}";
-
 var vm = new Vue({
-    el: '#tsubit',
-    data () {
-        return {
-            info: null,
-            loading: true,
-            errored: false
-        }
-    },
-    mounted () {
-        axios
-        .get('https://www.simce.cl/ficha2019/jsonSimce/ficha-521_fc45d51fd1e7d6711e0c45e552e2a0e2.json')
-        //.get('https://www.simce.cl/ficha'+ fecha +'/' + archivo)
-        .then(response => {
-            this.info = response.data.valores
-        })
-        .catch(error => {
-            console.log(error)
-            this.errored = true
-        })
-        .finally(() => this.loading = false)
-    }
+    el: '#tsubit'
 });    
 
 </script>
--- public/content/themes/templates_compiled/bd027f96c61c4aaacf725311586d757199d9ae86_0.file.index.tpl.php
+++ public/content/themes/templates_compiled/bd027f96c61c4aaacf725311586d757199d9ae86_0.file.index.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.38, created on 2021-03-07 01:56:34
+/* Smarty version 3.1.38, created on 2021-03-02 20:53:24
   from 'D:\xampp\htdocs\since\public\content\themes\templates\index.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.38',
-  'unifunc' => 'content_604432d2927025_15147271',
+  'unifunc' => 'content_603ea5c48ae966_85543326',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'bd027f96c61c4aaacf725311586d757199d9ae86' => 
     array (
       0 => 'D:\\xampp\\htdocs\\since\\public\\content\\themes\\templates\\index.tpl',
-      1 => 1615082131,
+      1 => 1614718276,
       2 => 'file',
     ),
   ),
@@ -20,35 +20,15 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
     'file:header_footer/head.tpl' => 1,
     'file:componenetes/header.tpl' => 1,
-    'file:componenetes/bloque_tipo_usuario.tpl' => 1,
-    'file:componenetes/bloque_resultado_since.tpl' => 1,
     'file:componenetes/pie_pagina.tpl' => 1,
     'file:header_footer/footer.tpl' => 1,
     'file:script_vuejs/index.tpl' => 1,
   ),
 ),false)) {
-function content_604432d2927025_15147271 (Smarty_Internal_Template $_smarty_tpl) {
+function content_603ea5c48ae966_85543326 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender('file:header_footer/head.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 $_smarty_tpl->_subTemplateRender('file:componenetes/header.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
-
-<section v-if="errored">
-    <p>Lo sentimos, no es posible obtener la información en este momento, por favor intente nuevamente mas tarde</p>
-</section>
-
-<div class="container-fluid" v-else><!-- Panel Cuerpo-->
-    <div v-if="loading">Cargando...</div>
-    <!--Información interna de la vista-->
-    <div class="container"v-else><!--Contenedor Interno-->
-
-        <?php $_smarty_tpl->_subTemplateRender("file:componenetes/bloque_tipo_usuario.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
-?>
-        <?php $_smarty_tpl->_subTemplateRender("file:componenetes/bloque_resultado_since.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
-?>
-
-    </div><!--Contenedor Interno-->
-    <!--Información interna de la vista-->
-</div><!-- Panel Cuerpo-->
 
 <?php $_smarty_tpl->_subTemplateRender('file:componenetes/pie_pagina.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 $_smarty_tpl->_subTemplateRender('file:header_footer/footer.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
